3b8ddb15f4bf7ac8ccb4f0701aa5a1c15fa903e4
Aldo Barrera <abarrera@bamaja.com.mx>
2015-07-07 18:21:00 -0500

    Fortune module

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var fortune = require('./lib/fortune.js');
 
 var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
@@ -11,18 +12,11 @@ app.get('/', function(req, res){
 	res.render('home');
 });
 app.get('/about', function(req, res){
-var randomFortune =
-fortunes[Math.floor(Math.random() * fortunes.length)];
-res.render('about', { fortune: randomFortune });
+//var randomFortune =
+//fortunes[Math.floor(Math.random() * fortunes.length)];
+//res.render('about', { fortune: randomFortune });
+res.render('about', { fortune: fortune.getFortune() });
 });
-
-var fortunes = [
-"Conquer your fears or they will conquer you.",
-"Rivers need springs.",
-"Do not fear what you don't know.",
-"You will have a pleasant surprise.",
-"Whenever possible, keep it simple.",
-];
 
 app.use(express.static(__dirname + '/public'));
 
